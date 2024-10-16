2a66e5de5c673cbcc1608897af3262ca8d55ec77
Moath Alhajri <moath.alhajrii@gmail.com>
2024-10-16 07:28:58 +0300

    update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,12 +1,5 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
-alias reloadshell="omz reload"
-alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
-alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-alias compile="commit 'compile'"
-alias version="commit 'version'"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -17,9 +10,8 @@ alias sites="cd $HOME/Herd"
 # Laravel
 alias a="herd php artisan"
 alias fresh="herd php artisan migrate:fresh --seed"
-alias tinker="herd php artisan tinker"
+alias tin="herd php artisan tinker"
 alias seed="herd php artisan db:seed"
-alias serve="herd php artisan serve"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
@@ -29,12 +21,6 @@ alias php="herd php"
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run dev"
-
-# Docker
-alias docker-composer="docker-compose"
-
-# SQL Server
-alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
 
 # Git
 alias gs="git status"
